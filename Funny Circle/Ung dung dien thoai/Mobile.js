2812848741a1808a3ecd5f2cc9237fe9b3c894ea
quanghung112 <laiquanghung112@gmail.com>
2019-05-16 17:17:18 +0700

    Hoan thien bai ung dung may tinh

--- Funny Circle/Ung dung dien thoai/Mobile.js
+++ Funny Circle/Ung dung dien thoai/Mobile.js
@@ -1,109 +1,144 @@
-let Mobile = function () {
-    this.name = "";
-    this.ison = true;
+let Mobile = function (name) {
+    this.name = name;
+    this.status = true;
     this.batery = 100;
     this.msg = '';
     this.inbox = [];
     this.outbox = [];
-    this.setname = function (name) {
-        this.name = name;
-    };
-    this.setison = function (ison, batery) {
-        if (batery <= 0) {
-            ison = false;
+    this.checkStatus = function () {
+        if (this.ison) {
+            console.log('Dang bat');
         } else {
-            this.ison = ison;
+            console.log('Dang tat')
         }
+    };
+    this.getStatus = function () {
+        return this.status;
+    };
+    this.getBatery = function () {
+        return this.batery;
+    };
+    this.setBatery = function (batery) {
+        this.batery = batery
+    };
+    this.turnOn = function () {
+        this.status = true;
+    };
+    this.turnOff = function () {
+        this.status = false;
     };
     this.charge = function () {
-        if (this.batery == 0) {
-            this.batery = 100;
+        if (this.batery < 100) {
+            this.batery += 1;
         }
     };
-    this.writeMsg = function () {
-        let letter = document.getElementById('Msg').value;
-        if (letter != '') {
-            this.msg = letter;
-            return this.msg;
-        }
+    this.writeMsg = function (msg) {
+        this.msg = msg;
     };
-    this.sendMsg = function () {
+    this.sendMsg = function (phone) {
+        phone.inbox.push(this.msg);
         this.outbox.push(this.msg);
     };
-    this.Inbox = function () {
-        this.inbox = this.outbox
+    this.readInbox = function () {
+        return this.inbox
     };
-    this.Outbox = function () {
-        if (this.name=='Samsung'){
-            document.getElementById('Outbox').innerHTML = this.outbox.join('\n');
-        }
+    this.readOutbox = function () {
+        return this.outbox
     };
 };
-let Samsung = new Mobile();
-Samsung.setname('Samsung');
-let dem = 0;
-let batery = parseInt(document.getElementById('batery').innerHTML);
-let arrMsg = [];
 
-function isOn() {
-    let iSon = Samsung.ison;
-
-    if (iSon) {
-        document.getElementById('isOn').value = 'Off';
-    } else {
-        document.getElementById('isOn').value = 'On';
-    }
-    dem++;
-    if (dem % 2 == 0) {``
-        Samsung.setison(true, batery);
-    } else {
-        Samsung.setison(false, batery);
-    }
-}
-
-function Send() {
-    let leter = document.getElementById('Msg').value;
-    if (leter != '') {
-        Samsung.writeMsg();
-        let Msg = Samsung.msg;
-        Samsung.sendMsg();
-        Samsung.Outbox();
-        arrMsg.push(Msg);
-        document.getElementById('Inbox1').innerHTML = arrMsg.join('\n');
-        document.getElementById('Msg').value = '';
+function Send(moblie, phone) {
+    let batery = moblie.batery;
+    if (moblie.status) {
+        let leter = document.getElementById('Msg' + moblie.name).value;
+        if (leter != '') {
+            moblie.writeMsg(leter);
+            moblie.sendMsg(phone);
+            let inbox = phone.readInbox();
+            let outbox = moblie.readOutbox();
+            if (phone.status) {
+                document.getElementById('Inbox' + phone.name).innerHTML = inbox.join('\n');
+            }
+            document.getElementById('Msg' + moblie.name).value = '';
+            document.getElementById('Outbox' + moblie.name).innerHTML = outbox.join('\n');
+        }
+        batery = batery - 1;
+        moblie.setBatery(batery);
+        document.getElementById('batery' + moblie.name).innerHTML = batery;
     }
 }
 
-// let Iphone = new Mobile();
-// let bateryI = parseInt(document.getElementById('batery1').innerHTML);
-// let arrMsgI = [];
-// let dem1 = 0;
-//
-// function isOn1() {
-//     let iSon = Iphone.ison;
-//
-//     if (iSon) {
-//         document.getElementById('isOn1').value = 'Off';
-//     } else {
-//         document.getElementById('isOn1').value = 'On';
-//     }
-//     dem1++;
-//     if (dem1 % 2 == 0) {
-//         Iphone.setison(true, bateryI);
-//     } else {
-//         Iphone.setison(false, bateryI);
-//     }
-// }
-//
-// function Send() {
-//     let leter = document.getElementById('Msg1').value;
-//     if (leter != '') {
-//         Iphone.writeMsg();
-//         let Msg = Samsung.msg;
-//         Iphone.sendMsg();
-//         Iphone.Outbox();
-//         arrMsgI.push(Msg);
-//         document.getElementById('Inbox').innerHTML = arrMsgI.join('\n');
-//         document.getElementById('Msg1').value = '';
-//     }
-// }
+function Batery(mobile) {
+    let batery=mobile.batery;
+    let a= setInterval(function (){
+            batery--;
+            mobile.setBatery(batery);
+            document.getElementById('batery' + mobile.name).innerHTML=batery;
+            if ( batery<1 || mobile.status===false) {
+                document.getElementById(mobile.name).value = 'Off'
+                clearInterval(a);
+        }
+    },800);
+}
+
+function Charge(mobile) {
+    if (mobile.batery <= 100) {
+        mobile.charge();
+        document.getElementById('batery' + mobile.name).innerHTML = mobile.batery;
+    }
+}
+
+function Status(mobile) {
+    if (mobile.getBatery()>0 ) {
+        if (mobile.getStatus()) {
+            mobile.turnOff();
+            document.getElementById(mobile.name).value = 'Off'
+        } else {
+            mobile.turnOn();
+            document.getElementById(mobile.name).value = 'On';
+            Batery(mobile);
+        }
+
+    }
+}
+
+let XiaoMI = new Mobile('XiaoMI');
+let Hawei = new Mobile('Hawei');
+Batery(XiaoMI);
+Batery(Hawei);
+function isOnXiaoMI() {
+    Status(XiaoMI);
+}
+
+function isOnHawei() {
+    Status(Hawei);
+}
+
+function SendXiaoMi() {
+    Send(XiaoMI, Hawei);
+}
+
+function SendHawei() {
+    Send(Hawei, XiaoMI);
+}
+
+function ChargeXiaoMi() {
+    let ChargeMI = setInterval(function () {
+        Charge(XiaoMI);
+        if (XiaoMI.batery >= 100) {
+            clearInterval(ChargeMI)
+        }
+    }, 1000);
+}
+
+function ChargeHawei() {
+    let ChargeHawei = setInterval(function () {
+        Charge(Hawei);
+        if (Hawei.batery >= 100) {
+            clearInterval(ChargeHawei)
+        }
+    }, 1200);
+}
+
+
+
